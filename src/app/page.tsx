db6365e553aead49e4d596484df3138d58858d9a
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-12-06 17:20:55 -0300

    build: downgrade na versão do next para versão 14
    Conflito entre a versão do React suportada pelo next-auth e o Next 15. Ambas tem suporte para versão 18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import heroImg from "@/assets/hero.svg"
 
 export default function Home() {
   return (
-    <div>
-      <h1>DevControle</h1>
-    </div>
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">Atendimento, clientes</h1>
+
+      <Image 
+        src={heroImg} 
+        alt="Imagem Hero do DevControle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+
+    </main>
   );
 }
